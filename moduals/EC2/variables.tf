1f8ba2645fb76ca1a37ba71ce0eaf43155012c8f
YashJetani <yashjetani123@gmail.com>
2025-07-11 10:05:01 +0530

    Update variables.tf

--- moduals/EC2/variables.tf
+++ moduals/EC2/variables.tf
@@ -1,1 +1,14 @@
+variable "ami_id" {
+  description = "AMI to use for the instance"
+  type        = string
+}
 
+variable "instance_type" {
+  description = "Type of instance to start"
+  type        = string
+}
+
+variable "instance_name" {
+  description = "Name tag for the instance"
+  type        = string
+}
